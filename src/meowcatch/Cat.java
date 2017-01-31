f973a157285f0076db9859cdd3cf7a7b33708f20
Catherine Leung <catherineleung123@gmail.com>
2017-01-30 23:28:31 -0800

    improved it more

--- src/meowcatch/Cat.java
+++ src/meowcatch/Cat.java
@@ -71,7 +71,7 @@ public class Cat {
             centerX = 61;
         }
 
-        rectangle.setRect(centerX - 55, centerY - 40, 60, 60);
+        rectangle.setRect(centerX - 30, centerY - 30, 60, 60);
     }
 
     public void moveRight() {
