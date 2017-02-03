a49aa93bb4d1d56824ceef0b075256ac86f61715
Catherine Leung <catherineleung123@gmail.com>
2017-02-02 21:07:32 -0800

    more code cleanup

--- src/meowcatch/Background.java
+++ src/meowcatch/Background.java
@@ -13,31 +13,11 @@ public class Background {
         speedX = 0;
     }
 
-    public void update() {
-
-    }
-
     public int getBgX() {
         return bgX;
     }
 
-    public void setBgX(int bgX) {
-        this.bgX = bgX;
-    }
-
     public int getBgY() {
         return bgY;
-    }
-
-    public void setBgY(int bgY) {
-        this.bgY = bgY;
-    }
-
-    public int getSpeedX() {
-        return speedX;
-    }
-
-    public void setSpeedX(int speedX) {
-        this.speedX = speedX;
     }
 }
